acc2e7af7107d8e5749023c32c03dcf119492093
Gorka Prieto <jtppragg@ehu.eus>
2020-02-24 16:50:12 +0100

    Ejemplos del tema 6

---
+++ T06 - Paquetes y API/EjemploImport/src/eus/euskadi/opendata/Meteo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eus.euskadi.opendata;
+
+/**
+ *
+ * @author gorka
+ */
+public class Meteo {
+    public String getCalidadAire() {
+        return "buena";
+    }
+}
---
+++ T06 - Paquetes y API/EjemploJerarquia/src/eus/ehu/ped/sub/Ejemplo1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eus.ehu.ped.sub;
+
+/**
+ *
+ * @author gorka
+ */
+public class Ejemplo1 {
+    
+}
